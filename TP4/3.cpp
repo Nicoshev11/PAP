49d709ba082e242c1e0cc796893ba6ad3d50c5ee
Lucas Tavolaro Ortiz <tavo92@gmail.com>
2016-12-04 22:27:42 -0300

    Codigo andando del 3 y listo

--- TP4/3.cpp
+++ TP4/3.cpp
@@ -6,7 +6,7 @@
 using namespace std;
 
 struct Punto {
-  float x, y;
+  int x, y;
 
   bool operator < (Punto p) {
     if (x != p.x)
@@ -42,35 +42,19 @@ int distancia(Punto a, Punto b)  {
 // Compara dos puntos por angulo, teniendo en cuenta el pivot que tomamos
 // como referencia
 bool comp_angulo(Punto a, Punto b) {
-  int cuenta = ccw(pivot, a, b);
-  // Si son colineares, nos quedamos con el que este mas cerca
-  if (cuenta == 0)
-    return distancia(pivot, a) < distancia(pivot, b);
-  return cuenta > 0;
+  return ccw(pivot, a, b) > 0;
 }
 
 float sign(Punto p1, Punto p2, Punto p3) {
   return (p1.x - p3.x) * (p2.y - p3.y) - (p2.x - p3.x) * (p1.y - p3.y);
 }
 
-bool gre0(float v) {
-  return v >= 0.0f || fabs(v) < 1e-7;
-}
-
-bool puntoEnTriangulo(Punto p, Punto p1, Punto p2, Punto p3) {
-  float alpha = ((p2.y - p3.y)*(p.x - p3.x) + (p3.x - p2.x)*(p.y - p3.y)) /
-        ((p2.y - p3.y)*(p1.x - p3.x) + (p3.x - p2.x)*(p1.y - p3.y));
-  float beta = ((p3.y - p1.y)*(p.x - p3.x) + (p1.x - p3.x)*(p.y - p3.y)) /
-       ((p2.y - p3.y)*(p1.x - p3.x) + (p3.x - p2.x)*(p1.y - p3.y));
-  float gamma = 1.0f - alpha - beta;
-
-  return gre0(alpha) && gre0(beta) && gre0(gamma);
-/*  bool b1, b2, b3;
-  cout << sign(pt, v1, v2) << " " << sign(pt, v2, v3) << " " << sign(pt, v3, v1) << endl;
+bool puntoEnTriangulo(Punto pt, Punto v1, Punto v2, Punto v3) {
+  bool b1, b2, b3;
   b1 = sign(pt, v1, v2) < 0.0f;
   b2 = sign(pt, v2, v3) < 0.0f;
   b3 = sign(pt, v3, v1) < 0.0f;
-  return ((b1 == b2) && (b2 == b3));*/
+  return ((b1 == b2) && (b2 == b3));
 }
 
 // Calcula la cantidad de puntos dentro del triangulo pivot, i, j
@@ -99,12 +83,10 @@ int calc(int i, int j) {
   int& ret = dp[i][j];
   if (ret != -1) return ret;
 
-  cout << "Calc " << i << " " << j << endl;
-
   ret = 0;
   for (int k = j+1; k < ph.size(); ++k) {
+    if (ccw(ph[i], ph[j], ph[k]) <= 0) continue;
     int rr = puntosDentro(j, k);
-    cout << rr << " " << i << " " << j << " " << k << endl;
     if (rr != -1) {
       ret = max(ret, calc(j, k) + rr + 1);
     }
@@ -123,7 +105,7 @@ void setPivot(Punto p) {
 }
 
 int main() {
-  freopen("3.in", "r", stdin);
+  //freopen("3.in", "r", stdin);
 
   // leo entrada
   cin >> h >> e;
@@ -149,9 +131,6 @@ int main() {
     for (int i = 1; i < ph.size(); ++i) {
       ret = max(ret, calc(0, i) + 2);
     }
-    /*for (int i = 0; i < ph.size(); ++i) {
-      cout << ph[i].x << " " << ph[i].y << endl;
-    }*/
   }
   cout << ret << endl;
   return 0;
