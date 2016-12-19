73d152bbda717317feab7d1d1b364abef6f10446
matiasvt <matiasvt@users.noreply.github.com>
2016-12-19 17:15:36 -0300

    Update ej1_tp4.py
    cambios en el print del formato y el signo del p cruz para el sort

--- TP4/ej1_tp4.py
+++ TP4/ej1_tp4.py
@@ -1,27 +1,35 @@
 def pcruz(o, a, b):
-    return (a[0] - o[0]) * (b[1] - o[1]) - (a[1] - o[1]) * (b[0] - o[0])
+    return ( o[0] -a[0]) * (-b[1] + o[1]) - (-a[1] + o[1]) * (-b[0] + o[0])
 
 def comparar(x,y): #compara segun producto cruz
-    pc=pcruz(pivot,x,y)    
-    if pc>0: #si el producto es positivo x va antes que y
+    pc=pcruz(pivot,x,y)
+    if pc<0: 
         return -1
-    if pc<0:
+    if pc>0:
         return 1
     else:
+#        return 0
         if x[1]<y[1]: #si son colineares primero devuelve el de menor coord y
-            return 1
+            return -1
         else:#son el mismo punto
-            return 0
-    
+            return 1
+#            if x[0]<y[0]:
+#                return -1
+#            else:
+#                return 0
+#    
 
 
-n=int((raw_input().strip())[0])
+n=int((raw_input()))
+#print n
 puntos=[]
 for i in xrange(n-2):
     P= map(int, raw_input().split())
     for i in range(0,6,2):
-        if ((P[i],P[i+1])) not in puntos:
-            puntos.append((P[i],P[i+1]))
-
+         puntos.append((P[i],P[i+1]))
 pivot=min(puntos) #lo usa la funcion de comparacion
-print sorted(puntos,cmp=comparar)
+a=set(puntos)
+a.remove(pivot)
+print pivot[0],pivot[1],
+for elem in sorted(a,cmp=comparar):
+    print elem[0],elem[1],
